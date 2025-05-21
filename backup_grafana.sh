82ffaf9920ff5482cc13ac07093070287588324b
bbr-mbernard <144031844+bbr-mbernard@users.noreply.github.com>
2025-05-21 16:52:11 +0200

    Update backup_grafana.sh

--- backup_grafana.sh
+++ backup_grafana.sh
@@ -3,4 +3,3 @@ docker stop c_grafana
 mkdir -p /root/grafana/backup/
 rsync -a /var/lib/docker/volumes/v_grafana/_data/ /root/grafana/backup/
 docker start c_grafana
-zenity --info --text="✅ Sauvegarde terminée."
